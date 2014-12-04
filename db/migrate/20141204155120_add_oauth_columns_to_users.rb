5f1aafca43f217c32b370f3bf801a1888546ead1
Ashwin Dey <deyashwin@gmail.com>
2014-12-04 12:14:04 -0500

    had to change old migration for heroku...

--- db/migrate/20141204155120_add_oauth_columns_to_users.rb
+++ db/migrate/20141204155120_add_oauth_columns_to_users.rb
@@ -3,6 +3,6 @@ class AddOauthColumnsToUsers < ActiveRecord::Migration
     add_column :users, :provider_name,    :string,   default: ""
     add_column :users, :provider_uid,     :string,   default: ""
     add_column :users, :oauth_token,      :string,   default: ""
-    add_column :users, :oauth_expires_at, :datetime, default: ""
+    add_column :users, :oauth_expires_at, :datetime
   end
 end
